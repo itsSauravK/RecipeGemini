a05194213f10149c09aceb321331f8962ab96580
maximuusS <ksaurav0211@gmail.com>
2024-03-10 00:00:37 -0500

    Added cors policy and connected api to home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,30 @@
+import { useEffect, useState } from "react";
+
 const Home = () => {
-  return (
-    <div>
-      <h1>Home</h1>
-      <p>Welcome to the home page!</p>
-    </div>
-  );
-}
+    const [recipes, setRecipes] = useState([]);
+    const [loading, setLoading] = useState(false);
+    async function getRecipes() {
+        setLoading(true)
+        const result = await fetch(`${process.env.REACT_APP_API_URL}/getAllRecipe`);
+        const response = await result.json();
+        setRecipes(response);
+        setLoading(false);
+        console.log(recipes);
+    }
+    useEffect(() => {
+        getRecipes();
+    }, []);
+    return (
+        <div>
+            <h1>Home</h1>
+            <p>Welcome to the home page!</p>
+            {loading ? <p>Loading ...</p> : recipes.map((recipe, index) => (
+                <div key={index}>
+                    <p>{recipe.name}</p>
+                </div>
+            ))}
+        </div>
+    );
+    }
 
 export default Home;
