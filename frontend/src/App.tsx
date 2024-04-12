755745c5d629ce828cc64226caf148501ad42f56
maximuusS <ksaurav0211@gmail.com>
2024-04-12 16:05:51 -0400

    Added lazy loading

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,31 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home.tsx";
-import Gemini from "./pages/Gemini";
-import RecipeDetails from "./pages/RecipeDetails.tsx";
-import UpdateRecipe from "./pages/UpdateRecipe.tsx";
 import ErrorBoundary from "./component/ErrorBoundary";
 import "./index.css";
+const Gemini = lazy(() => import("./pages/Gemini"));
+const RecipeDetails = lazy(() => import("./pages/RecipeDetails.tsx"));
+const UpdateRecipe = lazy(() => import("./pages/UpdateRecipe.tsx"));
+
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <ErrorBoundary fallback={<p>Something went wrong</p>}>
-              <Home />
-            </ErrorBoundary>
-          }
-        />
-        <Route path="/recipe/:id" element={<RecipeDetails />} />
-        <Route path="/updateRecipe/:id" element={<UpdateRecipe />} />
-        <Route path="/generate" element={<Gemini />} />
-        <Route path="*" element={<Home />} />
-      </Routes>
+      <Suspense fallback={<p>Loading</p>}>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <ErrorBoundary fallback={<p>Something went wrong</p>}>
+                <Home />
+              </ErrorBoundary>
+            }
+          />
+          <Route path="/recipe/:id" element={<RecipeDetails />} />
+          <Route path="/updateRecipe/:id" element={<UpdateRecipe />} />
+          <Route path="/generate" element={<Gemini />} />
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </Suspense>
     </Router>
   );
 }
